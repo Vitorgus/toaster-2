ad1d51f7e965b9cdb83c97d36525e267d027fd23
Vitorgus <26413854+Vitorgus@users.noreply.github.com>
2020-06-12 00:41:59 -0300

    server command now guild only

--- src/commands/server.ts
+++ src/commands/server.ts
@@ -3,6 +3,7 @@ import { Command } from '../custom-client';
 const server: Command = {
     name: 'server',
     description: 'Display server info',
+    guildOnly: true,
     execute: message => {
         message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
     },
